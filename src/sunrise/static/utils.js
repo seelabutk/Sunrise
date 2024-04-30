433c2e0a3d2c070706795e157ca5c6be6362b07e
Reagan (Alex) Austin <71673490+raustin9@users.noreply.github.com>
2024-04-30 13:53:13 -0400

    add location to spatial conversion function (#5)
    * add location to spatial conversion function
    
    * fix return type and add attribution

---
+++ src/sunrise/static/utils.js
@@ -0,0 +1,35 @@
+export const EARTH_AVG_ALTITUDE = 6_371_000;
+
+export const convert_location_to_spatial = (
+    latitude,
+    longitude,
+    altitude
+) => {
+    // Thanks https://stackoverflow.com/a/72476578
+    
+    const phi = to_radians(latitude);
+    const theta = to_radians(longitude);
+
+    const rho = EARTH_AVG_ALTITUDE + altitude;
+
+    const x = Math.cos(phi) * Math.cos(theta) * rho;
+    const y = Math.cos(phi) * Math.sin(theta) * rho;
+
+    const z = Math.sin(phi) * rho;
+
+    // (Note there's some slightly arbitrary choices here in what each axis means...
+    // you might want 'y' to point at the north pole instead of 'z', for example.)
+    
+    // I do :)
+    // y, z = z, y
+
+    return { 
+        x: x, 
+        y: y, 
+        z: z 
+    };
+}
+
+const to_radians = (num) => {
+    return (num * Math.PI) / 180.0;
+}
