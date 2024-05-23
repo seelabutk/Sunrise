dd1fcae23bbc8532be5ffc4f5954d981319fe3cc
raustin9 <raustin9@vols.utk.edu>
2024-05-23 18:42:06 +0000

    add jsdoc comments for mission class

--- src/sunrise/static/mission.js
+++ src/sunrise/static/mission.js
@@ -9,7 +9,10 @@ export class Mission {
     current_point = null; // The current point that the index is pointing to. TODO: we might not need this in the future
     paused = true;
 
-    /// NOTE: what to do with 'alt'?
+    /**
+        * @constructor
+        * @param {string} name The name of the mission
+        */
     constructor(name) {
     // constructor(name, position_data) {
         this.name = name;
@@ -18,33 +21,57 @@ export class Mission {
         this.current_point = this.point_list[0];
     }
 
+    /**
+        * @description Add a point to the list of points in this mission
+        * @param {THREE.Vector3} point The point to append
+        */
     add_point(point) {
         this.point_list.push(point);
     }
 
+    /**
+        * @description Pause playing the animation
+        */
     pause() {
         this.paused = true;
     }
 
+    /**
+        * @description Unpause playing the animation
+        */
     unpause() {
         this.paused = false;
     }
 
+    /**
+        * @description See if this animation is currently being played
+        * @returns {bool} True if paused false otherwise
+        */
     is_paused() {
         return this.paused;
     }
 
+
+    /**
+        * @description Get the length of the list of points in the mission
+        * @returns {number} the amount of points in the mission
+        */
     length() {
         return this.point_list.length;
     }
 
+    /**
+        * @description Get the current index of this mission
+        * @returns {number} The index we are at in the mission
+        */
     current_index() {
         return this.index;
     }
 
-    /// Increment our index and return the "next" point in the list
-    /// This is used to go forwards from where we are in the path
-    /// @returns null when we are at end of list
+    /** 
+        * @description Increment our index and return the "next" point in the list. This is used to go forwards from where we are in the path
+        * @returns null when we are at end of list
+        */
     next() {
         // Return early if we are at the end of the list
         if (this.index == this.point_list.length) {
@@ -57,13 +84,18 @@ export class Mission {
         return this.current_point;
     }
 
+    /**
+        * @description Get the amount of points remaining in the list
+        * @returns {Number} The number of positions yet to be played
+        */
     remaining_length() {
         return this.point_list.length - this.index;
     }
 
-    /// Decremement our index and return the "previous" point
-    /// This is used to go backwards from where we are in the path
-    /// @returns null when we are at beginning of list
+    /**
+        * @description Decremement our index and return the "previous" point. This is used to go backwards from where we are in the path
+        * @returns null when we are at beginning of list
+        */
     back() {
         if (this.index == 0) {
             return null;
@@ -75,8 +107,9 @@ export class Mission {
         return this.current_point;
     }
 
-    /// Move forwrads in the path
-    /// Return null when we are at end of the path
+    /**
+        * @description Move forwrads in the path Return null when we are at end of the path
+        */
     forward(offset) {
         // Return early when at the end of the list
         if (this.index === this.point_list.length-1 || this.paused === true) {
@@ -102,8 +135,10 @@ export class Mission {
         };
     }
 
-    /// Move forwrads in the path
-    /// Return null when we are at end of the path
+    /** 
+        * @description Move forwrads in the path
+        * @returns {null | THREE.Vector3} null when we are at end of the path
+        */
     backward(offset) {
         // Return early when at the end of the list
         if (this.index === 0 || this.paused === true) {
@@ -128,6 +163,11 @@ export class Mission {
         };
     }
 
+
+    /**
+        * @description Get render information for specific point 
+        * @returns {Object} The information for the current position, target, and up vector
+        */
     goto_point(index) {
         if (index > this.point_list.length - 1) {
             console.error(`Index ${index} out of bounds of this mission`);
@@ -140,8 +180,11 @@ export class Mission {
         }
     }
 
-    /// Create the button element to be placed in the DOM where
-    /// the App class wants it
+    /**
+        * @description Create the button for this mission
+        * @param {Function} callback The callback function for when this button is clicked
+        * @returns {Element} the button
+        */
     get_button(callback) {
         let btn = document.createElement("button");
         btn.id = `mission_${this.name.toLowerCase()}`;
@@ -152,7 +195,12 @@ export class Mission {
         return btn;
     }
 
-    /// Average the values of each component of the coordinates +- the specified range
+    /** 
+        * @description Average the values of each component of the coordinates +- the specified range 
+        * @param {Number} index The index of the point we want to average
+        * @param {Number} range The range of points on either side of `index` that we want to average
+        * @returns {THREE.Vector3} Vector3 with values of the averages
+        */
     #mean_position(index, range) {
         let mean_x = 0;
         let mean_y = 0;
