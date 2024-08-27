2843f6f2156f8746303d3eae0cbb589ccde4e341
Tanner Hobson <thobson2@vols.utk.edu>
2024-08-27 13:06:25 +0000

    Save changes from trying to diagnose east-west problems

--- src/city/server.py
+++ src/city/server.py
@@ -195,7 +195,7 @@ async def get_scene(
 #        )
 #        what.make()
 
-        NUM_THREADS = 3
+        NUM_THREADS = 1
         # NUM_THREADS = 6
         for _ in range(NUM_THREADS):
             what=scene.City(
